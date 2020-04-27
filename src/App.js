b80504de27a9c21e1c16a89594eb8257f3728930
Deep Kateja <kd.kateja@gmail.com>
2020-04-28 05:25:34 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import './App.css';
 import Menu from './Components/MenuComponents';
+import {DISHES} from './shared/dishes';
 
-function App()  {
+class App extends React.Component  {
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes:DISHES
+    };
+  }
+  render(){
   return (
    <div className = "App">
      <Navbar dark color = "primary">
@@ -12,9 +20,10 @@ function App()  {
          <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
        </div>
        </Navbar> 
-       <Menu />
+       <Menu dishes = {this.state.dishes}/>
    </div>
   );
+}
 }
 
 export default App;
