655f18abd35effc158fd58b89654d5ee8ba60c0b
Deep Kateja <kd.kateja@gmail.com>
2020-05-04 04:44:10 +0530

    Week 2 Assignment

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -4,6 +4,7 @@ import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { PROMOTIONS } from '../shared/promotion';
@@ -50,6 +51,7 @@ class Main extends Component {
           <Route exact path="/menu" component = {() => <Menu dishes = {this.state.dishes} />} />
           <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path="/contactus" component = {Contact}/>
+          <Route path = "/aboutus" component = {() => <About leaders = {this.state.leaders}/>}/>
           <Redirect to ="/home"/>
         </Switch>
         <Footer />
