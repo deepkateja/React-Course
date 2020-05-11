80911a0dcfd9bb9240e7dccd8bf8fc18e27bceb7
Deep Kateja <kd.kateja@gmail.com>
2020-05-12 01:48:09 +0530

    Redux Form Revisited

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -36,10 +36,10 @@ function Home(props) {
                     <RenderCard item={props.dish} isLoading={props.dishesLoading} errMess={props.dishesErrMess}/>
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard item={props.promotion} isLoading={props.dishesLoading} errMess={props.dishesErrMess}/>
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.leader} />
+                    <RenderCard item={props.leader} isLoading={props.dishesLoading} errMess={props.dishesErrMess}/>
                 </div>
             </div>
         </div>
